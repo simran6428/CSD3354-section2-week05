85cc57a8abda63cde6ca1cae3660dac77dae23e5
visualstudio <visualstudio@122-PC18>
2019-02-06 09:27:36 -0500

    gfh

---
+++ 730317/730317/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _730317
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            MethodA();
+        }
+      public  static void MethodA()
+        {
+            while (peanut())
+            {
+                Console.WriteLine("Help! i am stuck in an infintie loop!");
+            }
+        }
+        public static bool peanut()
+        {
+            return true;
+        }
+
+    }
+
+}
+
