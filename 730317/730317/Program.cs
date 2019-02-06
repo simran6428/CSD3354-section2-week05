856441979f6b3119202431c068bf22b92ca4eaa8
visualstudio <visualstudio@122-PC18>
2019-02-06 10:30:58 -0500

    fd

--- 730317/730317/Program.cs
+++ 730317/730317/Program.cs
@@ -8,21 +8,30 @@ namespace _730317
 {
     class program
     {
+        static int i = 0;
         static void Main(string[] args)
         {
             MethodA();
+            Console.ReadLine();
         }
       public  static void MethodA()
         {
             while (peanut())
             {
-                Console.WriteLine("Help! i am stuck in an infintie loop!");
-            }
+                Console.WriteLine("i is {0}", i);
+                if ( i > 10)
+                    {
+                        return;
+                    }
+            }     
         }
         public static bool peanut()
         {
+            i++;
             return true;
         }
+        
+
 
     }
 
